565f28774cef83779c24e0f8befc34a59dfc1f08
Michael Vietluzhskykh <mishavetl@gmail.com>
2018-12-05 15:52:49 +0200

    fix navigation offset bug

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function construct_children(headings, i, parent) {
     let html = "";
     for (; i < headings.length; i++) {
@@ -7,15 +6,15 @@ function construct_children(headings, i, parent) {
 
         if (i < headings.length - 1 && headings[i].tagName[1] < headings[i + 1].tagName[1]) {
             html +=
-                '<a class="navigation-link p-1 nav-link" ' + 'href="#' + headings[i].id + '">'
+                '<a class="navigation-link p-1 nav-link" ' + 'href="#map-' + headings[i].id + '">'
                 + headings[i].innerText + '</a>'
-                + '<nav class="nav nav-pills flex-column">'
+                + '<nav class="nav nav-pills flex-column">';
             let result = construct_children(headings, i + 1, headings[i]);
             html += result.html + '</nav>';
             i = result.i;
         } else {
             html +=
-                '<a class="navigation-link p-1 nav-link" ' + 'href="#' + headings[i].id + '">'
+                '<a class="navigation-link p-1 nav-link" ' + 'href="#map-' + headings[i].id + '">'
                 + headings[i].innerText +
                 '</a>';
         }
@@ -27,6 +26,14 @@ function construct_children(headings, i, parent) {
 const navigation = document.querySelector("#_navigation-container");
 
 if (navigation != null) {
-    const headings = document.querySelectorAll("h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]");
+    let headings = document.querySelectorAll("h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]");
     navigation.innerHTML += construct_children(headings, 0, {tagName: 'h0', id: ''}).html;
+    let navbarHeight = document.querySelector('#navbar').clientHeight - 1;
+    headings.forEach(heading => {
+        heading.innerHTML +=
+            '<em id="map-' + heading.id + '"' +
+            '   class="navigation-header-map"' +
+            '   style="position: relative; top: -' + navbarHeight + 'px;"' +
+            '>map</em>';
+    });
 }
